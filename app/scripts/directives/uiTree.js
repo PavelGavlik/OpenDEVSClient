dcf8c942435e9bfd84c49338b94af1ad7c80eeca
Pavel Gavlík <gav.pavel@gmail.com>
2013-03-03 15:00:59 +0100

    atomic explorer improvements, uiTree html template

--- app/scripts/directives/uiTree.js
+++ app/scripts/directives/uiTree.js
@@ -1,18 +1,19 @@
 'use strict';
 
-clientApp
-.directive('uiTree', function($compile) {
+clientApp.directive('uiTree', function($compile, $http, $templateCache) {
 	function linkFn(scope, element) {
-		if (!scope.val || !angular.isArray(scope.val.components))
-			return;
-		
-		// needs explicit compiling because of tree structure
-		element.append('<ul class="nav nav-list">' +
-			'<h2 ng-show="title">{{title}}</h2>' +
-			'<li ng-repeat="item in val.components" ng-class="{closed: !this[$index], end: !item.components.length}">' +
-			'<a ng-click="this[$index] = !this[$index]" ng-dblclick="itemDblclick(item)"><span>{{item.name}}</span></a>' +
-			'<div ui-tree ng-model="item" item-dblclick="itemDblclick"></div></li></ul>');
-		$compile(element.contents())(scope.$new());
+		$http.get('/templates/directives/uiTree.html',
+		{ cache: $templateCache }).success(function(template) {
+			// end recursion when reached item with no subitems
+			if (!scope.val || !angular.isArray(scope.val.components))
+				return;
+			
+			// needs explicit compiling because of tree structure
+			element.append(template);
+			$compile(element.contents())(scope.$new());
+		});
+
+
 	}
 
 	return {
--- test/spec/directives/uiTree.js
+++ test/spec/directives/uiTree.js
@@ -1,18 +1,34 @@
 'use strict';
 
 describe('Directive: uiTree', function() {
-	var element, $scope, $compile;
+	var element, $scope, $compile, $httpBackend;
 
 	beforeEach(module('clientApp'));
-	beforeEach(inject(['$rootScope', '$compile', function($r, $c) {
+	beforeEach(module('templates/directives/uiTree.html'));
+	beforeEach(inject(['$rootScope', '$compile', '$httpBackend', '$templateCache', function($r, $c, $h, $templateCache) {
 		$scope = $r;
 		$compile = $c;
+		$httpBackend = $h;
+
+		var template = $templateCache.get('templates/directives/uiTree.html');
+		$httpBackend.when('GET', '/templates/directives/uiTree.html').respond(template);
 	}]));
 
-	it('should compile', function() {
-		element = angular.element('<div ui-tree model="tree" />');
+	function build() {
+		element = angular.element('<div ui-tree ng-model="tree" />');
 		$compile(element)($scope);
-		$scope.tree = [{name: 'aa'}, {name: 'bb', components: [{name: 'cc'}]}];
 		$scope.$digest();
+		$httpBackend.flush();
+	}
+
+	it('should compile', function() {
+		$scope.tree = {};
+		build();
+	});
+
+	it('should fill tree from data', function() {
+		$scope.tree = {components: [{name: 'aa'}, {name: 'bb'}]};
+		build();
+		expect(element.find('li').length).toEqual(2);
 	});
 });
