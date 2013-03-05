3b4492c618219874e057d8175285760e71bfe5ee
Pavel Gavlík <gav.pavel@gmail.com>
2013-03-05 21:39:30 +0100

    add jsdoc + little refactor

--- app/scripts/directives/uiCoupledExplorer.js
+++ app/scripts/directives/uiCoupledExplorer.js
@@ -71,82 +71,155 @@ clientApp.factory('computeSubmodelSize', ['submodelPortGap', function(submodelPo
 }]);
 
 clientApp.factory('computeCouplingSegments', function() {
-	function pointsDist(p1, p2) {
-		var dx = p2.x - p1.x;
-		var dy = p2.y - p1.y;
-		return Math.sqrt(dx*dx + dy*dy);
-	}
-	function pointsSum(p1, p2) {
-		return {
-			x: p1.x + p2.x,
-			y: p1.y + p2.y
-		};
-	}
-	function pointsDifference(p1, p2) {
-		return {
-			x: p1.x - p2.x,
-			y: p1.y - p2.y
-		};
-	}
-	function pointTimes(p, num) {
-		return {
-			x: p.x * num,
-			y: p.y * num
-		};
-	}
-	function pointDivided(p, num) {
-		return {
-			x: p.x / num,
-			y: p.y / num
-		};
-	}
-	function pointsInterpolate(p1, p2, ammount) {
-		return pointsSum(p1, pointTimes(pointsDifference(p2, p1), ammount));
-	}
-	function pointsEqual(p1, p2) {
-		return p1.x === p2.x && p1.y === p2.y;
+	/** @type {{x: number, y: number}} */
+	var PointRecord;
+
+	/**
+	 * Class representing a point
+	 * @param {number} x
+	 * @param {number} y
+	 * @constructor
+	 */
+	function Point(x, y) {
+		/** @type {number} */
+		this.x = x;
+		/** @type {number} */
+		this.y = y;
 	}
 
+	/**
+	 * Creates new Point instance from regular object
+	 * @param {PointRecord} p
+	 * @return {Point}
+	 */
+	Point.fromObject = function(p) {
+		return new Point(p.x, p.y);
+	};
+
+	/**
+	 * Returns new point which is sum of this point and p
+	 * @param {Point} p
+	 * @return {Point}
+	 */
+	Point.prototype.plus = function(p) {
+		return new Point(this.x + p.x, this.y + p.y);
+	};
+
+	/**
+	 * Computes difference of two points
+	 * @param {Point} p
+	 * @return {Point}
+	 */
+	Point.prototype.minus = function(p) {
+		return new Point(this.x - p.x, this.y - p.y);
+	};
+
+	/**
+	 * Computes distance between this and p
+	 * @param {Point} p
+	 * @return {number}
+	 */
+	Point.prototype.distTo = function pointsDist(p) {
+		var dx = p.x - this.x;
+		var dy = p.y - this.y;
+		return Math.sqrt(dx*dx + dy*dy);
+	};
+
+	/**
+	 * Computes product of this point and number
+	 * @param {number} num
+	 * @return {Point}
+	 */
+	Point.prototype.timesNum = function(num) {
+		return new Point(this.x * num, this.y * num);
+	};
+
+	/**
+	 * Computes division of this point and number
+	 * @param {number} num
+	 * @return {Point}
+	 */
+	Point.prototype.divideByNum = function(num) {
+		return new Point(this.x / num, this.y / num);
+	};
+
+	/**
+	 * Interpolation between two points with given ammount
+	 * @param {Point} p
+	 * @param {number} ammount
+	 * @return {Point}
+	 */
+	Point.prototype.interpolateTo = function(p, ammount) {
+		return this.plus(p.minus(this).timesNum(ammount));
+	};
+
+	/**
+	 * Return true if coordinates of two points are equal
+	 * @param {Point} p
+	 * @return {boolean}
+	 */
+	Point.prototype.isEqual = function(p) {
+		return this.x === p.x && this.y === p.y;
+	}
+
+
+	/**
+	 * Returns a line segment of SVG path
+	 * @param {PointRecord|Point} p
+	 * @return {string}
+	 */
 	function lineSegment(p) {
 		return [' L', p.x, p.y].join(' ');
 	}
+
+	/**
+	 * Returns a bezier segment of SVG path
+	 * @param {PointRecord|Point} p1
+	 * @param {PointRecord|Point} p2
+	 * @return {string}
+	 */
 	function bezierSegment(p1, p2) {
 		return [' Q', p1.x, p1.y, p2.x, p2.y].join(' ');
 	}
 
+	/**
+	 * Computes connected SVG path from points it should go through
+	 * @param {Array<PointRecord>} vertices
+	 * @return {string}
+	 */
 	function computeSegments(vertices) {
 		// algorithm adapted from NcLineMorph#computeSegments
 		var path = '';
 		if (vertices.length === 2)
 			path = lineSegment(vertices[0]) + lineSegment(vertices[1]);
 
-		var v2, v3, lastUsed = vertices[0];
+		var v2, v3, lastUsed = Point.fromObject(vertices[0]);
 		for (var i = 1; i < vertices.length - 1; i++) {
-			v2 = vertices[i];
-			v3 = vertices[i + 1];
-			var l1 = pointsDist(lastUsed, v2);
-			var l2 = pointsDist(v2, v3);
+			v2 = Point.fromObject(vertices[i]);
+			v3 = Point.fromObject(vertices[i + 1]);
+			var l1 = lastUsed.distTo(v2);
+			var l2 = v2.distTo(v3);
 			var symmetry = l1 - l2;
 			var m, end;
 
 			if (symmetry < 0) {
-				m = pointDivided(pointsSum(lastUsed, v2), 2);
-				end = pointsInterpolate(v2, v3, l1 / l2 / 2);
-				if (!pointsEqual(lastUsed, m))
+				m = lastUsed.plus(v2).divideByNum(2);
+				end = v2.interpolateTo(v3, l1 / l2 / 2);
+				if (!lastUsed.isEqual(m))
 					path += lineSegment(lastUsed) + lineSegment(m);
 				path += bezierSegment(v2, end);
 				lastUsed = end;
 			}
 			else {
-				m = pointDivided(pointsSum(v2, v3), 2);
-				end = pointsInterpolate(v2, lastUsed, l2 / l1 / 2);
-				if (!pointsEqual(lastUsed, end))
+				m = v2.plus(v3).divideByNum(2);
+				end = v2.interpolateTo(lastUsed, l2 / l1 / 2);
+				if (!lastUsed.isEqual(end))
 					path += lineSegment(lastUsed) + lineSegment(end);
 				path += bezierSegment(v2, m);
 				lastUsed = m;
 			}
 		}
-		if (v3 && !pointsEqual(lastUsed, v3))
+		if (v3 && !lastUsed.isEqual(v3))
 			path += lineSegment(v3);
 
 		return 'M' + path.substr(2);
